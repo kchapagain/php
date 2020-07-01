ce0043ea425478d079b3182c1ad11bd52167bdb1
Kamal Chapagain <kamal@iseekplant.com.au>
2020-07-02 09:17:46 +1000

    update

--- readpngFilesFromDir.php
+++ readpngFilesFromDir.php
@@ -7,57 +7,29 @@
         Created on: 07/03/2015
 */
 
-//function to read files form directory and add them in an array
-    
-function readFileFromDir($dirName){    
-    
-    //dir to read image from 
-    
+function readFileFromDir($dirName)
+{
     $dir = $dirName;
-    //$dir = "../images/";
-    
     $images = array();
-    
-    $i = 0; //index for array
+    $i = 0;
+    if (is_dir($dir)) {
 
-
-
-        //check to see if directory is present  
-        //Open a directory, and read its contents
-            if (is_dir($dir)){
-                
-                //opend dir
-                if ($dh = opendir($dir)){
-   
-                     while (($file = readdir($dh)) !== false){
-                         
-                           $parts = explode('.', $file); //seperate file name and extension
-
-                            $extension = array_pop($parts); //get file extention
-                            
-                            //check the extension
-                            if( $extension == 'png'){
-                                
-                               //display only if file is .png
-                                    echo  $file ."[" .$i. " ] <br>" ;
-                                    
-                                    $images[$i] = $file; //add element to array 
-                                    
-                                    $i++; //increase array element by 1
-                                
-                            }//end if
-
-                    }//end while
-                    
-                    //close dir
-                    closedir($dh);
-                }//end reading dir
-                
-            }//end if
-            
-            else {
-                echo "No Dir found";
+        if ($dh = opendir($dir)) {
+            while (($file = readdir($dh)) !== false) {
+                $parts = explode('.', $file);
+                $extension = array_pop($parts);
+                if ($extension == 'png') {
+                    //display only if file is .png
+                    echo $file . "[" . $i . " ] <br>";
+                    $images[$i] = $file;
+                    $i++;
+                }
             }
-}//end function
+            closedir($dh);
+        }
+    } else {
+        echo "No Dir found";
+    }
+}
 
 readFileFromDir("../images/");
